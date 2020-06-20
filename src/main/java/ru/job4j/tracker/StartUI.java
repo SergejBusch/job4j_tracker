9429e347e9c122ce3bdd2532ddfd4b91a6865f76
Mr. Anderson <h-blast@list.ru>
2020-06-20 08:01:26 +0200

    5. Tracker - storage [#305515]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item firstItem = new Item();
+        Item secondItem = new Item();
+        Item thirdItem = new Item();
+        firstItem.setId(1);
+        firstItem.setName("first");
+        secondItem.setId(2);
+        secondItem.setName("second");
+        thirdItem.setId(3);
+        thirdItem.setName("third");
+        Tracker tracker = new Tracker();
+        tracker.add(firstItem);
+        tracker.add(secondItem);
+        tracker.add(thirdItem);
+        System.out.println(tracker.findById(2).getName());
+    }
+}
