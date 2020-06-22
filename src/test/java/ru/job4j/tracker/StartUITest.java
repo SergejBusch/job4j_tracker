43030367fd5158054675b1ce45ba6c232589ae8f
Mr. Anderson <h-blast@list.ru>
2020-06-22 03:35:47 +0200

    5. Input and polymorphism. [#305451]

---
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class StartUITest {
+    @Test
+    public void whenAddItem() {
+        String[] answers = {"first"};
+        Input input = new StubInput(answers);
+        Tracker tracker = new Tracker();
+        StartUI.createItem(input, tracker);
+        Item created = tracker.findAll()[0];
+        Item expected = new Item("first");
+        Assert.assertThat(created.getName() , is(expected.getName()));
+    }
+}
