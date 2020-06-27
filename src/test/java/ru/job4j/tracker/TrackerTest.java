657823188d86a2b62476becef66576b65d4ce023
Mr. Anderson <h-blast@list.ru>
2020-06-28 00:40:14 +0200

    0. Sorting [#305424]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,6 +1,13 @@
 package ru.job4j.tracker;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.IsNull.nullValue;
@@ -38,5 +45,31 @@ public class TrackerTest {
         int id = bug.getId();
         tracker.delete(id);
         assertThat(tracker.findById(id), is(nullValue()));
+    }
+
+    @Test
+    public void whenIncreaseSort() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("first");
+        Item item2 = new Item("second");
+        Item item3 = new Item("third");
+        tracker.add(item3);
+        tracker.add(item2);
+        tracker.add(item1);
+        Collections.sort(tracker.findAll(),new IncreaseItemSort());
+        Assert.assertThat(tracker.findAll(), is(new ArrayList<>(List.of(item1, item2, item3))));
+    }
+
+    @Test
+    public void whenDecreaseSort() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("first");
+        Item item2 = new Item("second");
+        Item item3 = new Item("third");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Collections.sort(tracker.findAll(),new DecreaseItemSort());
+        Assert.assertThat(tracker.findAll(), is(new ArrayList<>(List.of(item3, item2, item1))));
     }
 }
