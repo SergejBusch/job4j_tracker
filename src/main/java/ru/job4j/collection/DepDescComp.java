b31396c1d9acb3596ff6379173e76eee9b367bd2
Mr. Anderson <h-blast@list.ru>
2020-07-01 00:24:15 +0200

    Sort the departments [#305417]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        String e1 = o1.split("/")[0];
+        String e2 = o2.split("/")[0];
+        if (e1.equals(e2)) {
+            return o1.compareTo(o2);
+        } else {
+            return o2.compareTo(o1);
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,29 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+
+    public static List<String> fillGaps(List<String> deps) {
+        HashSet<String> tmp = new HashSet<>();
+        for (String value : deps) {
+            String start = "";
+            for (String el : value.split("/")) {
+                start += "/" + el;
+                tmp.add(start.substring(1));
+            }
+        }
+        List<String> list = new ArrayList<>(tmp);
+        sortAsc(list);
+        System.out.println(list);
+        return list;
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        orgs.sort(Comparator.naturalOrder());
+    }
+
+    public static void sortDesc(List<String> orgs) {
+        orgs.sort(new DepDescComp());
+    }
+}
