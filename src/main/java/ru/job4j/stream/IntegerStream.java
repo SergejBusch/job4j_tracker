00ce32814e3c17a7bdffdd6981d5fc2fcdf435da
Mr. Anderson <h-blast@list.ru>
2020-07-04 03:30:30 +0200

    0. Stream API [#305282]

---
+++ src/main/java/ru/job4j/stream/IntegerStream.java
@@ -0,0 +1,16 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class IntegerStream {
+    public static void main(String[] args) {
+        List<Integer> integerList = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            integerList.add(i);
+        }
+        integerList = integerList.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
+        System.out.println(integerList);
+    }
+}
