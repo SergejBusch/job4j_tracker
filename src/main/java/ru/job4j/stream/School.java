5f77763d537652df0c3bf7af1cb994ad4b33ca17
Mr. Anderson <h-blast@list.ru>
2020-07-04 14:38:37 +0200

    4. Converting List to Map. [#305286]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -1,6 +1,7 @@
 package ru.job4j.stream;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -10,6 +11,17 @@ public class School {
         return students.stream()
                 .filter(predict)
                 .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        Map<String, Student> studentMap = (List.of(
+                new Student("Smith", 50),
+                new Student("Fisher", 66))
+                .stream()
+                .collect(Collectors.toMap(
+                                Student::getSurname,
+                                student -> student
+                )));
     }
 }
 
